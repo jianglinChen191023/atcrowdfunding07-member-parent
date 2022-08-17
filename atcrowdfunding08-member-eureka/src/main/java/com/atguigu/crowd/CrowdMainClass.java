dd00bd77d3ee4f78738ad6bba7aba34abbd581d2
jianglinChen191023 <1912623646@qq.com>
2022-08-18 02:42:04 +0800

    `Eureka` 工程

---
+++ atcrowdfunding08-member-eureka/src/main/java/com/atguigu/crowd/CrowdMainClass.java
@@ -0,0 +1,19 @@
+package com.atguigu.crowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chenjianglin
+ * @date 2022/8/18 02:05
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class CrowdMainClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdMainClass.class, args);
+    }
+
+}
