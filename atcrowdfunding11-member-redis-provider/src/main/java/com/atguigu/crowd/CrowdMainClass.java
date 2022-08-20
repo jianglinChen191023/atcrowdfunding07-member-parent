3241b17b09fd3e15cdf0a0a0bab1be7af75a8474
jianglinChen191023 <1912623646@qq.com>
2022-08-20 08:42:51 +0800

    `Redis` 工程

---
+++ atcrowdfunding11-member-redis-provider/src/main/java/com/atguigu/crowd/CrowdMainClass.java
@@ -0,0 +1,17 @@
+package com.atguigu.crowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenjianglin
+ * @date 2022/8/19 11:05
+ */
+@SpringBootApplication
+public class CrowdMainClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdMainClass.class, args);
+    }
+
+}
---
+++ atcrowdfunding11-member-redis-provider/src/test/java/com/atguigu/crowd/RedisTest.java
@@ -0,0 +1,31 @@
+package com.atguigu.crowd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author chenjianglin
+ * @date 2022/8/19 11:14
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    // private Logger logger = LoggerFactory.getLogger(RedisTest.class);
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    public void testSet(){
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+
+        operations.set("apple", "red");
+    }
+
+}
