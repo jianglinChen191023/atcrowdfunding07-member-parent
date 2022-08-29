aa5042a4b46d03e086307eafd2f9a0841af500c3
jianglinChen191023 <1912623646@qq.com>
2022-08-29 10:40:27 +0800

    package com.atguigu.crowd.test;

---
+++ atcrowdfunding12-member-authentication-consumer/src/test/java/com/atguigu/crowd/test/CrowdTest.java
@@ -0,0 +1,58 @@
+package com.atguigu.crowd.test;
+
+import com.aliyun.api.gateway.demo.util.HttpUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 陈江林
+ * @date 2022/8/29 09:16
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrowdTest {
+
+    @Test
+    public void testSendMessage() {
+        String host = "https://dfsns.market.alicloudapi.com";
+        String path = "/data/send_sms";
+        String method = "POST";
+        String appcode = "1948fa6afc674ea3bc2fea47f36e1108";
+        Map<String, String> headers = new HashMap<>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        //根据API的要求，定义相对应的Content-Type
+        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        Map<String, String> querys = new HashMap<>();
+        Map<String, String> bodys = new HashMap<>();
+        bodys.put("content", "code:1234");
+        bodys.put("phone_number", "185xxxx5080");
+        bodys.put("template_id", "TPL_0000");
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            // System.out.println(response.toString());
+            // 获取response的body
+            // {"status":"OK","request_id":"TIDef01a498c96747a19aa166cd81f9279d"}
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
